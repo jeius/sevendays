f5ac6d3b3bbc525d931fca7aec996be04c8cbc1f
Jeius <pahamajulius@gmail.com>
2026-08-31 21:35:46 +0800

    feat: package inclusion schema discriminated by kind

--- packages/types/src/index.ts
+++ packages/types/src/index.ts
@@ -1,5 +1,6 @@
 export * from './appointment.js';
 export * from './attire.js';
 export * from './branch.js';
+export * from './inclusion.js';
 export * from './package.js';
 export * from './print-size.js';
